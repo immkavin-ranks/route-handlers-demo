e13bd99377d81e3093927ae758bb6f7b7cdefe94
immkavin-ranks <immkavin.ofcl@gmail.com>
2025-01-21 21:13:51 +0530

    Headers in route handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,11 @@
-export async function GET() {
+import { type NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+
+    const requestHeaders = new Headers(request.headers)
+
+    console.log(requestHeaders.get('Authorization'));
+    
+
     return new Response("Profile data");
 }
