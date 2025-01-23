56621bfef8b883b1552d880a9c7ec849aca7ee94
immkavin-ranks <immkavin.ofcl@gmail.com>
2025-01-23 16:55:08 +0530

    Add caching and revalidation for time route; implement categories route

--- src/app/time/route.ts
+++ src/app/time/route.ts
@@ -1,3 +1,15 @@
+export const dynamic = 'force-static' // test caching
+
+
+// revalidate cached data with incremental static regeneration
+export const revalidate = 10 // revalidate every request in seconds
+
 export async function GET () {
   return Response.json({ time: new Date().toLocaleTimeString() })
 }
+
+/*
+ * Caching only works for GET requests,
+ * POST, PUT, PATCH, and DELETE requests will not be cached.,
+ *
+ */
