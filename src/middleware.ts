255391418fc1ac663ca6c2085442d5c95c216eac
immkavin-ranks <immkavin.ofcl@gmail.com>
2025-01-23 18:52:01 +0530

    Rewrite instead of rewrite

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ export function middleware (request: NextRequest) {
 //   return NextResponse.redirect(new URL('/', request.url))
 
     if (request.nextUrl.pathname === '/profile') {
-        return NextResponse.redirect(new URL('/hello', request.nextUrl))
+        return NextResponse.rewrite(new URL('/hello', request.nextUrl))
     }
 }
 
