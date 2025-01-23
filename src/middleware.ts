f94ff6ca19c0f1b64c545f639c1b5f54ce04cb7d
immkavin-ranks <immkavin.ofcl@gmail.com>
2025-01-23 20:09:28 +0530

    cookies and headers in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,22 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware (request: NextRequest) {
-//   return NextResponse.redirect(new URL('/', request.url))
+  const response = NextResponse.next()
 
-    if (request.nextUrl.pathname === '/profile') {
-        return NextResponse.rewrite(new URL('/hello', request.nextUrl))
-    }
+  const themePreference = request.cookies.get('theme')
+
+  if (!themePreference) {
+    response.cookies.set('theme', 'dark')
+  }
+
+  response.headers.set('Custom-Header', 'custom value')
+
+  return response
+
+  //   return NextResponse.redirect(new URL('/', request.url))
+  // if (request.nextUrl.pathname === '/profile') {
+  //     return NextResponse.rewrite(new URL('/hello', request.nextUrl))
+  // }
 }
 
 // export const config = {
