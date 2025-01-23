742808f1b3d4f0e010d18d0718f7bdc0359354e8
immkavin-ranks <immkavin.ofcl@gmail.com>
2025-01-23 18:50:44 +0530

    Refactor middleware to use conditional statements

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,13 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware (request: NextRequest) {
-  return NextResponse.redirect(new URL('/', request.url))
+//   return NextResponse.redirect(new URL('/', request.url))
+
+    if (request.nextUrl.pathname === '/profile') {
+        return NextResponse.redirect(new URL('/hello', request.nextUrl))
+    }
 }
 
-export const config = {
-    matcher: '/profile'
-}
+// export const config = {
+//     matcher: '/profile'
+// }
